57e5c4b9035a80ba595067873a9c5a699a01bfef
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-06-18 20:51:31 +0200

    01_c: Getting Started
    f2

---
+++ src/app/booking-history/booking-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+
+@Component({
+  selector: 'app-booking-history',
+  imports: [CommonModule, SharedModule],
+  templateUrl: './booking-history.component.html',
+  styleUrls: ['./booking-history.component.scss']
+})
+export class BookingHistoryComponent {}
